68fdabeb2c06cb3e0a54acc913954e2c4203b537
Cody Murray <cdillon.murray@yahoo.com>
2022-04-06 00:05:42 -0500

    added moment.js method to get current date

--- cody.js
+++ cody.js
@@ -1,3 +1,8 @@
+// function for getting the current day. 
+//  function for color coding the hours of the day based on current time,....
+//   ....to show past, present, and future by color. 
+
+
 $(document).ready(function () {
-    $("")
-})
+    $("#currentDay").text(moment().format("MMM Do YY")); 
+});
