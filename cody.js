f61d988dfbe2da6470664a6061ed8c37b5c0a602
Cody Murray <cdillon.murray@yahoo.com>
2022-04-06 18:05:57 -0500

    psuedo code updated for js file

--- cody.js
+++ cody.js
@@ -6,3 +6,25 @@
 $(document).ready(function () {
     $("#currentDay").text(moment().format("MMM Do YY")); 
 });
+
+
+
+// save user input to local storage  , 
+// set onclick event for the save button for every hour
+// add eventlistner to save data to local storage, localStorage,setitem("myItem",value)
+
+localStorage.setitem("hour1",)
+
+
+
+
+// color code function to represent past, present, and future by color
+// function to pull in the current time ("hour")&("number") 
+// then grab all of the elements with ".time-block" as a class 
+
+// create a for loop for each of these items 
+// items meaning each element with the class="time-block"
+
+// if the 
+// if the time is = to the current time present 
+// if the time tiem is before assign the future class to it. 
