3883a5fca22b74474c034d95efa4f8fae94dee3f
Cody Murray <cdillon.murray@yahoo.com>
2022-04-08 00:09:32 -0500

    updated readme

--- cody.js
+++ cody.js
@@ -1,14 +1,12 @@
-// document will get the element by id, then its textContent will = moment()
-// will get the current day 
+//will give the current date at the top of the page 
 document.getElementById("currentDay").textContent = moment().format("MMM Do YY");
 
 
-// grab the current hour, save into a variable 
-// grab all the elements with time-block 
-// loop over those elemenets
-// comparing the element hour to the current hour 
 
+
+// grab the current hour, save into a variable 
 const currentHour = moment().hour()
+// grab all the elements with time-block 
 const timeBlocks = document.getElementsByClassName("time-block")
 for( let i = 0; i < timeBlocks.length; i++) {
     const block = timeBlocks.item(i)
@@ -23,6 +21,7 @@ for( let i = 0; i < timeBlocks.length; i++) {
         localStorage.setItem(blockHour, task.value)
         console.log(blockHour, task.value)
     })
+    // comparing the element hour to the current hour 
     if(blockHour < currentHour) {
         block.className += " past "
     }else if(blockHour == currentHour) {
@@ -30,56 +29,5 @@ for( let i = 0; i < timeBlocks.length; i++) {
     }else if(blockHour > currentHour) {
         block.className += " future "
     }
-    // else if for foe 
-    // else if statement for equal to current hour 
-    
 };
 
-
-
-
-// save user input to local storage  , 
-// set onclick event for the save button for every hour
-// add eventlistner to save data to local storage, localStorage,setitem("myItem",value)
-// var saveButton = document.querySelector("#save");
-// var task = document.querySelector("#task")
-
-
-// saveButton.addEventListener("click", function (event) {
-//     event.preventDefault();
-
-//     var todo = {
-//         task: task.value.trim()
-//     }
-
-//     localStorage.setItem("9:00", JSON.stringify(todo));
-// });
-
-
-
-
-// localStorage.setItem("10:00", JSON.stringify(todo));
-// localStorage.setItem("11:00", JSON.stringify(todo));
-// localStorage.setItem("12:00", JSON.stringify(todo));
-// localStorage.setItem("13:00", JSON.stringify(todo));
-// localStorage.setItem("14:00", JSON.stringify(todo));
-// localStorage.setItem("15:00", JSON.stringify(todo));
-// localStorage.setItem("16:00", JSON.stringify(todo));
-// localStorage.setItem("17:00", JSON.stringify(todo));
-// localStorage.setItem("9:00", JSON.stringify(todo));
-// localStorage.setItem("9:00", JSON.stringify(todo));
-// localStorage.setItem("9:00", JSON.stringify(todo));
-
-
-
-
-// color code function to represent past, present, and future by color
-// function to pull in the current time ("hour")&("number") 
-// then grab all of the elements with ".time-block" as a class 
-
-// create a for loop for each of these items 
-// items meaning each element with the class="time-block"
-
-// if the 
-// if the time is = to the current time present 
-// if the time tiem is before assign the future class to it. 
